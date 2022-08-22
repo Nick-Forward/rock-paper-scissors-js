fe66d98b6c8880d4340468b1889c55613c708c9b
Nick-Forward <nickforward17@gmail.com>
2022-08-21 18:39:56 -0600

    Commit 1.1 Added comments to script.js Broke down logical structure of rock-paper-scissors

---
+++ script.js
@@ -0,0 +1,10 @@
+// Adding Comments here to map out logical progression of function
+//Author: Nick Forward. Date: Aug 21 2022
+//Step 1: Build function to generate computer input for a game
+//Step 2: Build function to get input from player
+//Step 3: Build function to run game
+//Step 4: Build function to run game if player chooses rock
+//Step 5: Build function to run game if player chooses scissors
+//Step 6: Build function to run game if player chooses paper
+//Step 7: Output result to player
+//Step 8: Build function for best of 5
