53b98d3c540232706024e0ed92dbd0e2652eeb7d
Nick-Forward <nickforward17@gmail.com>
2022-08-24 16:17:03 -0600

    Commit 1.3 Added function to take player input Added function to compare player input to computer simulation Added code to return result in console.log

--- script.js
+++ script.js
@@ -3,13 +3,65 @@
 //Step 1: Build function to generate computer input for a game
 let computerscore = 0;
 let playerscore = 0;
-function Computeroutput() {
-let choices = ['rock', 'paper', 'scissors']
-return choices[Math.floor(Math.random()*choices.length)]
+function Computerinput() {
+let choices = ['rock', 'paper', 'scissors'];
+let computeroutcome = choices[Math.floor(Math.random()*choices.length)];
+return computeroutcome;
+}
+//Step 2: Build function to get input from player
+function Playerinput () {
+    let input = window.prompt("Rock, Paper, or Scissors?");
+    let playeroutcome = input.toLowerCase();
+    return playeroutcome;
+}
+//Step 3: Build function to run game
+function compare (Player, Computer) {
+    if (Player == Computer) {
+        let result = 'tie game';
+        return result;
+    } else if (Player == 'rock'){
+        if (Computer == 'paper'){
+            let result = 'Computer wins';
+            return result;
+        }
+        else {
+            let result = 'Player wins';
+            return result;
+        }
+    }
+    else if (Player == 'paper') {
+        if (Computer == 'scissors'){
+            let result = 'Computer wins';
+            return result;
+        }
+        else {
+            let result = 'Player wins';
+            return result;
+        }
+    }
+    else if (Player == 'scissors') {
+        if (Computer == 'rock'){
+            let result = 'Computer wins';
+            return result;
+        }
+        else {
+            let result = 'Player wins';
+            return result;
+        }
+    }
+    else {
+        let result = 'invalid input';
+        return result;
+    }
 }
 
-//Step 2: Build function to get input from player
-//Step 3: Build function to run game
+function rungame(){
+    let input = window.prompt("Rock, Paper, or Scissors?");
+    let playeroutcome = input.toLowerCase();
+    let computeroutcome = Computerinput();
+    return compare(playeroutcome, computeroutcome);
+}
+console.log(rungame());
 //Step 4: Build function to run game if player chooses rock
 //Step 5: Build function to run game if player chooses scissors
 //Step 6: Build function to run game if player chooses paper
